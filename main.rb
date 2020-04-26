31937cea4c0e41722539bf3daf8b905bd2ac0283
zetzetn <53105202+zetzetn@users.noreply.github.com>
2020-04-26 21:59:09 +0900

    3-3 モンスターの攻撃処理実装

--- main.rb
+++ main.rb
@@ -1,30 +1,116 @@
 class Brave
-  # 複数の値を同時に指定することができる
   attr_reader :name, :offense, :defense
   attr_accessor :hp
 
-  # 引数に**を記述：ハッシュしか受け取れなくなる
-  def initialize(**params)
-    # 各パラメータをハッシュで取得
+  # 必殺攻撃の計算に使う定数
+  SPECIAL_ATTACK_CONSTANT = 1.5
+
+  def initialize(params)
     @name = params[:name]
     @hp = params[:hp]
     @offense = params[:offense]
     @defense = params[:defense]
   end
+
+  # 引数でモンスタークラスのインスタンスを受け取る
+  def attack(monster)
+    puts "#{@name}の攻撃"
+
+    attack_num = rand(4)
+
+    if attack_num == 0
+      # 必殺攻撃の表示
+      puts "必殺攻撃"
+      # calculate_special_attackの呼び出し
+      # 攻撃力の1.5倍の数値が戻り値として返ってくる
+      damage = calculate_special_attack - monster.defense
+    else
+      # 通常攻撃の表示
+      puts "通常攻撃"
+      damage = @offense - monster.defense
+    end
+
+
+    # 自己代入：monster.hpからdamageを引いた値をmonster.hpに代入
+    monster.hp -= damage
+
+    puts "#{monster.name}は#{damage}のダメージを受けた"
+    puts "#{monster.name}の残りHPは#{monster.hp}だ"
+  end
+
+  def calculate_special_attack
+    @offense * SPECIAL_ATTACK_CONSTANT
+  end
 end
 
-# パラメータの値をinitializeに渡す
-brave = Brave.new(name:"テリー", hp:500, offense:150, defense:100)
+class Monster
+  # nameを削除
+  attr_reader :offense, :defense
+  # nameを追加
+  attr_accessor :hp, :name
 
+  POWER_UP_RATE = 1.5
+  CALC_HALF_HP = 0.5
 
-  # ヒアドキュメント
-  puts <<~TEXT
-  NAME:#{brave.name}
-  HP:#{brave.hp}
-  OFFENSE:#{brave.offense}
-  DEFFENSE:#{brave.defense}
-  TEXT
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
 
-  brave.hp -= 30
+    @transform_flag = false
+    @trigger_of_transform = params[:hp] * CALC_HALF_HP
+  end
 
-  puts "#{brave.name}はダメージを受けた!　残りHPは#{brave.hp}だ"
+  def attack(brave)
+    if @hp <= @trigger_of_transform && @transform_flag == false
+      @transform_flag = true
+      transform
+    end
+    puts "#{@name}の攻撃"
+
+    damage = @offense - brave.defense
+    brave.hp -= damage
+
+    puts "#{brave.name}は#{damage}のダメージを受けた"
+    puts "#{brave.name}の残りHPは#{brave.hp}だ"
+  end
+
+  private
+
+    def transform
+      transform_name = "ドラゴン"
+      puts <<~EOS
+      #{@name}は怒っている
+      #{@name}は#{transform_name}に変身した
+      EOS
+
+      @offense *= POWER_UP_RATE
+      @name = transform_name
+    end
+
+end
+
+brave = Brave.new(name: "テリー", hp: 500, offense: 150, defense: 100)
+monster = Monster.new(name: "スライム", hp: 250, offense: 200, defense: 100)
+
+# attackメソッドの呼び出し
+brave.attack(monster)
+# 以下を削除
+# puts <<~TEXT
+#
+# HP：#{brave.hp}
+# ATTACK：#{brave.attack}
+# DEFENSE：#{brave.defense}
+#
+# TEXT
+
+# 以下を削除
+# puts <<~TEXT
+#
+# Monster
+# HP：#{monster.hp}
+# OFFENSE：#{monster.offense}
+# DEFENSE：#{monster.defense}
+#
+# TEXT
